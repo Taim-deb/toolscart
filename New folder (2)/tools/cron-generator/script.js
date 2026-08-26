a13ddbf2fa1e380383955e8bbf7fffc58fbe14bf
techboy <taimmahmoud50@gmail.com>
2026-08-26 20:13:11 +0400

    Delete New folder (2) directory

--- New folder (2)/tools/cron-generator/script.js
+++
@@ -1,1 +0,0 @@
-const $=s=>document.querySelector(s), T=window.ToolScart;$('#process').onclick=()=>{$('#out').value=[...document.querySelectorAll('[data-cron]')].map(i=>i.value.trim()||'*').join(' '); T.setStatus('status','Generated.','ok')}; $('#copy').onclick=()=>T.copy($('#out').value,'status'); $('#export').onclick=()=>T.download('cron.txt', $('#out').value); $('#clear').onclick=clearAll;
